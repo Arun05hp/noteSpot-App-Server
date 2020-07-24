60ae475d7fa8ea101a07486b83508c1931b8d782
Arun Kumar <kumar05.962013@gmail.com>
2020-07-24 22:18:41 +0530

    signup api

--- fetch/fetchData.js
+++ fetch/fetchData.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 const con = require("../mysql");
 
 // api to get all posts
-router.get("/", (req, res, next) => {
+router.get("/data", (req, res, next) => {
   con.query("SELECT * FROM user", (err, result) => {
     if (err) {
       res.json({ err: err });
@@ -14,14 +15,30 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/signup", (req, res, next) => {
-  res.send(req.body);
-  // con.query("SELECT * FROM user", (err, result) => {
-  //   if (err) {
-  //     res.json({ err: err });
-  //   } else {
-  //     res.json(result);
-  //   }
-  // });
+  const data = req.body;
+  const { email } = data;
+  con.query(
+    "SELECT COUNT(*) AS cnt FROM user WHERE email = ?",
+    email,
+    (err, result) => {
+      if (err) return res.status(422).send(err.message);
+      if (result[0].cnt > 0) {
+        return res.status(409).send("Email Already Exist ! Please Login");
+      } else {
+        con.query("INSERT INTO user SET ?", data, (err, result) => {
+          if (err) {
+            return res.status(422).send(err.message);
+          } else {
+            const token = jwt.sign(
+              { userId: result.insertId },
+              "User_Secret_Key"
+            );
+            return res.send({ token });
+          }
+        });
+      }
+    }
+  );
 });
 
 module.exports = router;
